dfb23b091a9bfb3b28dd02d778ecc8012f0c9b90
mah <m.h@duke.edu>
2021-01-31 14:24:56 -0500

    Implemented custom rsync command to allow skipping costly processing commands if no input has changed

--- analyze/code/master.sh
+++ analyze/code/master.sh
@@ -6,17 +6,28 @@ code_dir="$step/code/"
 in_dir="$step/in/"
 out_dir="$step/out/"
 
-# Copy necessary code and inputs to remote machine
-rsync $step/code/*.do "$REMOTE:${remote_dir}${code_dir}" &
-rsync $step/in/*.dta "$REMOTE:${remote_dir}${in_dir}" &
+##Sync necessary code and inputs to remote machine
+# First, store command: inserts a file-changed indicator into rsync log.
+indic="File changed!"
+my_rsync="rsync -a --log-file=\$(mktemp /tmp/rsync.log.${step}.XXXXXX) --log-file-format=\"${indic} %f %i\""
+# Remove old temp files created by this script
+rm /tmp/rsync.log.${step}.*
+# Sync code and data files, marking whether they changed
+eval $my_rsync "$step/code/*.do" "$REMOTE:${remote_dir}${code_dir}" &
+eval $my_rsync "$step/in/*.dta" "$REMOTE:${remote_dir}${in_dir}" &
 wait
-
-ssh -t $REMOTE << SCRIPT
-  cd $remote_dir$code_dir
-  stata run.do
+# Only run remote commands to produce output if some input has changed
+if fgrep "${indic}" /tmp/rsync.log.${step}.* > /dev/null; then
+    echo "rsync did something! Running commands..."
+    ssh -t $REMOTE << SCRIPT
+      cd $remote_dir$code_dir
+      stata run.do
 SCRIPT
+else
+  echo "rsync: All files were current. Skipping commands..."
+fi
 
-# Copy outputs back to local machine
+# Sync outputs back to local machine
 rsync "$REMOTE:${remote_dir}${out_dir}*.dta" $step/out/ &
 wait
 
