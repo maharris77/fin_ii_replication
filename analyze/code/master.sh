1e0726a6e9729663e7424068aab18693818f4d78
mah <m.h@duke.edu>
2021-01-30 23:02:28 -0500

    Begin analysis coding

---
+++ analyze/code/master.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+step="analyze"
+
+remote_dir="$REMOTE_HOME/workspace/fin_ii_replication/"
+code_dir="$step/code/"
+in_dir="$step/in/"
+out_dir="$step/out/"
+
+# Copy necessary code and inputs to remote machine
+scp *.do "$REMOTE:${remote_dir}${code_dir}" &
+scp ../in/*.dta "$REMOTE:${remote_dir}${in_dir}" &
+wait
+
+ssh -t $REMOTE << SCRIPT
+  cd $remote_dir$code_dir
+  stata run.do
+SCRIPT
+
+# Copy outputs back to local machine
+scp "$REMOTE:${remote_dir}${out_dir}*.dta" ../out/ &
+wait
+
+say "done"
---
+++ analyze/code/run.do
@@ -0,0 +1,4 @@
+**The compustat data need some modification to merge without error:
+pwd
+use "../in/out.dta", clear
+* Rename fyear and make gvkey numeric (why did Sufi do that?)
